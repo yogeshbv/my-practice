d2d22beb6d902208efd232018a00f0cc32b823cb
yogeshbv <yogeshbv09@gmail.com>
2017-07-17 00:02:24 +0530

    list and create with routing

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { CreateTodoComponent }  from './create.todo.component';
+import { ListTodoComponent }  from './list.todo.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,AppRoutingModule ],
+  declarations: [ AppComponent, CreateTodoComponent, ListTodoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
